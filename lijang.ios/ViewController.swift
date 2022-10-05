4fd1e048f6b3537e982cd76b51359141e6633c82
kylegav <60828879+kylegav@users.noreply.github.com>
2022-10-05 13:04:18 -0400

    Initial Commit

---
+++ lijang.ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lijang.ios
+//
+//  Created by Kyle Gavin on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
