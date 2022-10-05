69ba86a46dd76298cc73b63476cb74f5ccd45d57
kylegav <60828879+kylegav@users.noreply.github.com>
2022-10-05 13:28:32 -0400

    init login view controller, feed view controller + nav controller. associated swift files

---
+++ lijang.ios/View Controllers/feedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  feedViewController.swift
+//  lijang.ios
+//
+//  Created by Kyle Gavin on 10/5/22.
+//
+
+import UIKit
+
+class feedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
